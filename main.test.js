3c99cf9fa90b4a0c8051e95ba186d5024d72f196
paulportillo <yummta@gmail.com>
2021-09-28 00:08:26 -0500

    Fix test

--- main.test.js
+++ main.test.js
@@ -1,17 +1,19 @@
-const { getLastPosition } = require("./main");
+const { getLastPosition } = require("./getLastPosition");
+
+const WORDL_SIZE = [5, 3];
 
 const robot1 = [[1, 1, "E"], "RFRFRFRF"];
 const robot2 = [[3, 2, "N"], "FRRFLLFFRRFLL"];
 const robot3 = [[0, 3, "W"], "LLFFFLFLFL"];
 
 test("get last position into mars", () => {
-  expect(getLastPosition(robot1)).toEqual([1, 1, "E"]);
+  expect(getLastPosition(robot1, WORDL_SIZE)).toEqual([1, 1, "E"]);
 });
 
 test("lose the robot", () => {
-  expect(getLastPosition(robot2)).toEqual([3, 3, "N", "LOST"]);
+  expect(getLastPosition(robot2, WORDL_SIZE)).toEqual([3, 3, "N", "LOST"]);
 });
 
 test("avoid losing the robot thanks to the scent of the loss", () => {
-  expect(getLastPosition(robot3)).toEqual([2, 3, "S"]);
+  expect(getLastPosition(robot3, WORDL_SIZE)).toEqual([2, 3, "S"]);
 });
